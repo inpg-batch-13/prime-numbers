34de1cacb53676e47e4b4559a54366caf5355dec
rahmatrhd <kickyou44@gmail.com>
2019-06-19 20:18:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function isPrime(number) {
+
+}
+
+function primeNumbers(maxNumber) {
+
+}
+
+// TEST CASES:
+console.log(primeNumbers(5)) // [1, 2, 3, 5]
+console.log(primeNumbers(20)) // [1, 2, 3, 5, 7, 11, 13, 17, 19]
+console.log(primeNumbers(2)) // [1, 2]
